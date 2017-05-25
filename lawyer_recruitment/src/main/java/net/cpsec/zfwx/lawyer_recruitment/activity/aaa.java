53591b414130477d539974e659bb3d89ec640119
wuyaxiong <2578064861@qq.com>
2017-05-25 15:30:28 +0800

    222

--- lawyer_recruitment/src/main/java/net/cpsec/zfwx/lawyer_recruitment/activity/aaa.java
+++
@@ -1,9 +0,0 @@
-package net.cpsec.zfwx.lawyer_recruitment.activity;
-
-/**
- * Created by szh on 2017/5/25.
- */
-
-public class aaa {
-    /*asdasdas*/
-}
--- lawyer_recruitment/src/main/java/net/cpsec/zfwx/lawyer_recruitment/activity/bbb.java
+++
@@ -1,9 +0,0 @@
-package net.cpsec.zfwx.lawyer_recruitment.activity;
-
-/**
- * Created by admina on 2017/5/25.
- */
-
-public class bbb {
-   // 阿什顿发
-}
