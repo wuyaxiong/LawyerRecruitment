90b3ef5adb9567705fc9def019db3373d7a88efa
lice <450098886@qq.com>
2017-06-22 16:34:52 +0800

    @专家功能添加外部专家

--- lawyer_recruitment/src/main/java/net/cpsec/zfwx/guodian/activity/SendZhuanJiaActivity.java
+++ lawyer_recruitment/src/main/java/net/cpsec/zfwx/guodian/activity/SendZhuanJiaActivity.java
@@ -1,119 +1,155 @@
 package net.cpsec.zfwx.guodian.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
-import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.widget.ImageView;
 
-import com.alibaba.fastjson.JSON;
-import com.android.volley.manager.RequestMap;
-
 import net.cpsec.zfwx.guodian.R;
-import net.cpsec.zfwx.guodian.adapter.ZhuanJiaAdapter;
-import net.cpsec.zfwx.guodian.entity.ZhuanJiaBean;
-import net.cpsec.zfwx.guodian.ui.YRecycleview;
-import net.cpsec.zfwx.guodian.utils.NetUrl;
-import net.cpsec.zfwx.guodian.utils.Toast;
+import net.cpsec.zfwx.guodian.fragment.NeibuzhuanjiaFragment;
+import net.cpsec.zfwx.guodian.fragment.WaibuzhuanjiaFragment;
 
-import java.util.List;
-import java.util.Map;
+public class SendZhuanJiaActivity extends BaseActivity {
+//    private YRecycleview yRecycleview;
+//    private ZhuanJiaAdapter adapter;
+//    private ImageView iv_back;
+//    private int resultCode = 3;
+//    private boolean isRefreshState = true;//是否刷新
+//    private List<ZhuanJiaBean.InforBean> zhuanJiaInfor;
+//    private ZhuanJiaBean zhuanJiaBean;
 
-public class SendZhuanJiaActivity extends BaseActivity implements View.OnClickListener, YRecycleview.OnRefreshAndLoadMoreListener{
-    private YRecycleview yRecycleview;
-    private ZhuanJiaAdapter adapter;
     private ImageView iv_back;
-    private int resultCode = 3;
-    private boolean isRefreshState = true;//是否刷新
-    private List<ZhuanJiaBean.InforBean> zhuanJiaInfor;
-    private ZhuanJiaBean zhuanJiaBean;
+    private ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_zhuan_jia);
         initView();
-        initData();
     }
     private void initView() {
-        iv_back = (ImageView) findViewById(R.id.iv_back);
+        iv_back= (ImageView) findViewById(R.id.iv_back_zhuanjia);
         iv_back.setOnClickListener(this);
-        yRecycleview= (YRecycleview) findViewById(R.id.sendzj_zhuanjia);
-        yRecycleview.setRefreshAndLoadMoreListener(this);
-
+        viewPager= (ViewPager) findViewById(R.id.vp_zhuanjia);
+        tabLayout= (TabLayout) findViewById(R.id.tl_tab_zhuanjia);
+        SendZhuanJiaActivity.SectionsPagerAdapter adapter = new SendZhuanJiaActivity.SectionsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setCurrentItem(0);
     }
-    //请求数据
-    private void initData() {
-        RequestMap params = new RequestMap();
-        setParams(NetUrl.ZHUANJIA, params, 0);
-    }
-    //数据请求成功后数据处理方法
-    @Override
-    public void onSuccess(String response, Map<String, String> headers, String url, int actionId) {
-        super.onSuccess(response, headers, url, actionId);
-        try {
-            zhuanJiaBean = JSON.parseObject(response, ZhuanJiaBean.class);
-            Log.e("专家页面", "zhuanJiaBean"+zhuanJiaBean.toString() );
 
-            if (isRefreshState) {
-                yRecycleview.setReFreshComplete();
-                zhuanJiaInfor = zhuanJiaBean.getInfor();
-            } else {
-                zhuanJiaInfor = zhuanJiaBean.getInfor();
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = new Fragment();
+            switch (position) {
+                case 0:
+                    fragment = new NeibuzhuanjiaFragment();
+                    break;
+                case 1:
+                    fragment = new WaibuzhuanjiaFragment();
+                    break;
             }
-            setAdapter();
-        } catch (Exception e) {
-            Toast.prompt(this, "数据异常");
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "内部专家";
+                case 1:
+                    return "外部专家";
+
+            }
+            return null;
         }
     }
 
 
-    private void setAdapter() {
-        if (isRefreshState && null != zhuanJiaInfor) {
-            adapter = new ZhuanJiaAdapter(this, zhuanJiaInfor);
-            yRecycleview.setLayoutManager(new LinearLayoutManager(this));
-            yRecycleview.setAdapter(adapter);
-        } else {
-            adapter.notifyDataSetChanged();
-        }
-
-        //touPiaoInfor  =  [InforBean{vote_id=1, admin_id=1, title='最美国电人', time=1496735800, description='各分（子）公司、研究院、基层企业员工：
-        //目前，由国资委宣传局开展的“最美国电人”征集活动已进入公众投票阶段。在本次评选活动中，以下员工成功入选。为宣传具有国电特色的先进典型，
-        // 以典型宣传讲好“国电故事”，请按照有关要求，积极参与投票。'},
-        // InforBean{vote_id=2, admin_id=1, title='发表意见', time=1496735800, description='各部门积极发表意见'}]
-
-        adapter.setOnItemClickListener(new ZhuanJiaAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                //Intent intent=new Intent(SendZhuanJiaActivity.this, FaTieActivity.class);
-                ZhuanJiaBean.InforBean infor= zhuanJiaInfor.get(position-1);
-               // Bundle bundle=new Bundle();
-                Log.e("专家页面", "infor.getUsername()"+infor.getUsername() );
-               // bundle.putString("zhuanjia_name",infor.getUsername());
-                //intent.putExtras(bundle);
-
-                Intent mIntent = new Intent();
-                mIntent.putExtra("zhuanjia_name", infor.getUsername());
-                // 设置结果，并进行传送
-                //SendZhuanJiaActivity.this.setResult(RESULT_OK, mIntent);//RESULT_OK为自定义常量
-                SendZhuanJiaActivity.this.setResult(resultCode, mIntent);
-                SendZhuanJiaActivity.this.finish();
-            }
-        });
-    }
-
-
-    public void onRefresh() {
-        isRefreshState = true;
-        yRecycleview.setReFreshComplete();
-        //Toast.prompt(this, "刷新完成。测试阶段");
-    }
-
-    public void onLoadMore() {
-        isRefreshState = false;
-        initData();
-        yRecycleview.setNoMoreData(true);
-       // Toast.prompt(this, "没有更多数据。测试阶段");
-    }
+//    private void initView() {
+//        iv_back = (ImageView) findViewById(R.id.iv_back);
+//        iv_back.setOnClickListener(this);
+//        yRecycleview= (YRecycleview) findViewById(R.id.sendzj_zhuanjia);
+//        yRecycleview.setRefreshAndLoadMoreListener(this);
+//
+//    }
+//    //请求数据
+//    private void initData() {
+//        RequestMap params = new RequestMap();
+//        setParams(NetUrl.ZHUANJIA, params, 0);
+//    }
+//    //数据请求成功后数据处理方法
+//    @Override
+//    public void onSuccess(String response, Map<String, String> headers, String url, int actionId) {
+//        super.onSuccess(response, headers, url, actionId);
+//        try {
+//            zhuanJiaBean = JSON.parseObject(response, ZhuanJiaBean.class);
+//            Log.e("专家页面", "zhuanJiaBean"+zhuanJiaBean.toString() );
+//
+//            if (isRefreshState) {
+//                yRecycleview.setReFreshComplete();
+//                zhuanJiaInfor = zhuanJiaBean.getInfor();
+//            } else {
+//                zhuanJiaInfor = zhuanJiaBean.getInfor();
+//            }
+//            setAdapter();
+//        } catch (Exception e) {
+//            Toast.prompt(this, "数据异常");
+//        }
+//    }
+//
+//
+//    private void setAdapter() {
+//        if (isRefreshState && null != zhuanJiaInfor) {
+//            adapter = new ZhuanJiaAdapter(this, zhuanJiaInfor);
+//            yRecycleview.setLayoutManager(new LinearLayoutManager(this));
+//            yRecycleview.setAdapter(adapter);
+//        } else {
+//            adapter.notifyDataSetChanged();
+//        }
+//
+//
+//        adapter.setOnItemClickListener(new ZhuanJiaAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//                ZhuanJiaBean.InforBean infor= zhuanJiaInfor.get(position-1);
+//                Log.e("专家页面", "infor.getUsername()"+infor.getUsername() );
+//                Intent mIntent = new Intent();
+//                mIntent.putExtra("zhuanjia_name", infor.getUsername());
+//                // 设置结果，并进行传送
+//                SendZhuanJiaActivity.this.setResult(resultCode, mIntent);
+//                SendZhuanJiaActivity.this.finish();
+//            }
+//        });
+//    }
+//
+//
+//    public void onRefresh() {
+//        isRefreshState = true;
+//        yRecycleview.setReFreshComplete();
+//        //Toast.prompt(this, "刷新完成。测试阶段");
+//    }
+//
+//    public void onLoadMore() {
+//        isRefreshState = false;
+//        initData();
+//        yRecycleview.setNoMoreData(true);
+//       // Toast.prompt(this, "没有更多数据。测试阶段");
+//    }
 }
--- lawyer_recruitment/src/main/java/net/cpsec/zfwx/guodian/fragment/NeibuzhuanjiaFragment.java
+++ lawyer_recruitment/src/main/java/net/cpsec/zfwx/guodian/fragment/NeibuzhuanjiaFragment.java
@@ -1,18 +1,38 @@
 package net.cpsec.zfwx.guodian.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alibaba.fastjson.JSON;
+import com.android.volley.manager.RequestMap;
+
 import net.cpsec.zfwx.guodian.R;
+import net.cpsec.zfwx.guodian.adapter.ZhuanJiaAdapter;
+import net.cpsec.zfwx.guodian.entity.ZhuanJiaBean;
+import net.cpsec.zfwx.guodian.ui.YRecycleview;
+import net.cpsec.zfwx.guodian.utils.NetUrl;
+import net.cpsec.zfwx.guodian.utils.Toast;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NeibuzhuanjiaFragment extends Fragment {
+public class NeibuzhuanjiaFragment extends BaseFragment implements View.OnClickListener, YRecycleview.OnRefreshAndLoadMoreListener{
+    private YRecycleview yRecycleview;
+    private ZhuanJiaAdapter adapter;
+    private int resultCode = 3;
+    private boolean isRefreshState = true;//是否刷新
+    private List<ZhuanJiaBean.InforBean> zhuanJiaInfor;
+    private ZhuanJiaBean zhuanJiaBean;
 
 
     public NeibuzhuanjiaFragment() {
@@ -24,7 +44,83 @@ public class NeibuzhuanjiaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_neibuzhuanjia, container, false);
+        View v =inflater.inflate(R.layout.fragment_neibuzhuanjia, container, false);
+        initData();
+        initView(v);
+        return v;
+    }
+    private void initView(View v) {
+        yRecycleview= (YRecycleview) v.findViewById(R.id.sendzj_neibuzhuanjia);
+        yRecycleview.setRefreshAndLoadMoreListener(this);
+
+    }
+    //请求数据
+    private void initData() {
+        RequestMap params = new RequestMap();
+        setParams(NetUrl.ZHUANJIA, params, 0);
+    }
+    //数据请求成功后数据处理方法
+    @Override
+    public void onSuccess(String response, Map<String, String> headers, String url, int actionId) {
+        super.onSuccess(response, headers, url, actionId);
+        try {
+            zhuanJiaBean = JSON.parseObject(response, ZhuanJiaBean.class);
+            Log.e("专家页面", "zhuanJiaBean"+zhuanJiaBean.toString() );
+
+            if (isRefreshState) {
+                yRecycleview.setReFreshComplete();
+                zhuanJiaInfor = zhuanJiaBean.getInfor();
+            } else {
+                zhuanJiaInfor = zhuanJiaBean.getInfor();
+            }
+            setAdapter();
+        } catch (Exception e) {
+            Toast.prompt(getActivity(), "数据异常");
+        }
+    }
+
+
+    private void setAdapter() {
+        if (isRefreshState && null != zhuanJiaInfor) {
+            adapter = new ZhuanJiaAdapter(getActivity(), zhuanJiaInfor);
+            yRecycleview.setLayoutManager(new LinearLayoutManager(getActivity()));
+            yRecycleview.setAdapter(adapter);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
+
+
+        adapter.setOnItemClickListener(new ZhuanJiaAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                ZhuanJiaBean.InforBean infor= zhuanJiaInfor.get(position-1);
+                Log.e("专家页面", "infor.getUsername()"+infor.getUsername() );
+                Intent mIntent = new Intent();
+                mIntent.putExtra("zhuanjia_name", infor.getUsername());
+                // 设置结果，并进行传送
+                getActivity().setResult(resultCode, mIntent);
+                getActivity().finish();
+            }
+        });
+    }
+
+
+    public void onRefresh() {
+        isRefreshState = true;
+        yRecycleview.setReFreshComplete();
+        //Toast.prompt(this, "刷新完成。测试阶段");
+    }
+
+    public void onLoadMore() {
+        isRefreshState = false;
+        initData();
+        yRecycleview.setNoMoreData(true);
+        // Toast.prompt(this, "没有更多数据。测试阶段");
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
 }
